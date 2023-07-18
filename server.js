10a6ca7c94ebaf2b4e486045820a4bb33a6bce36
Nathanvititoe <nathanvititoe@gmail.com>
2023-07-18 12:20:35 -0700

    switch to deployed

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ dotenv.config();
 import pg from "pg";
 
 const { Pool } = pg;
-// const db_conn = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
-const client = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  database: "goshop",
+const db_conn = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+// const client = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   database: "goshop",
+// });
 
-const PORT = 3001;
-// const PORT = process.env.PORT;
+// const PORT = 3001;
+const PORT = process.env.PORT;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors({ origin: "*" }));
